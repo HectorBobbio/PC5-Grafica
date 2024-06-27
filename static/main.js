0e53633bd9e5a3bfab9ae9e6aeb30ba59848c798
Hector Bobbio <hbobbioh@uni.pe>
2024-06-27 12:25:55 -0500

    Test#3

--- static/main.js
+++ static/main.js
@@ -15,16 +15,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-    const rana = await loadGLTF('/static/assets/models/rana/scene.gltf');
+    const rana = await loadGLTF('/static/assets/models/chair/scene.gltf');
 
     //                      X     Y    Z
     rana.scene.scale.set(0.01, 0.01, 0.01);
+    //raccoon2.scene.scale.set(0.03, 0.03, 0.03);
     //                         X    Y   Z
     rana.scene.position.set(0, -0.4, 0);
-    
+    //raccoon2.scene.position.set(0,-0.4,0);
 
-    const anchor_rana = mindarThree.addAnchor(0);
-    anchor_rana.group.add(rana.scene);
+    const anchor_1 = mindarThree.addAnchor(0);
+    anchor_1.group.add(rana.scene);
+
+    //const anchor_2 = mindarThree.addAnchor(1);
+    //anchor_2.group.add(raccoon2.scene);
 
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
