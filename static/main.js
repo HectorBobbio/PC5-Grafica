f069295c505b8e11b6631c25680d3182aa96ec19
Hector Bobbio <hbobbioh@uni.pe>
2024-06-27 12:08:31 -0500

    Test rana

--- static/main.js
+++ static/main.js
@@ -8,29 +8,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '/static/assets/targets/luka-libro.mind',
+      imageTargetSrc: '/static/assets/targets/test-sapo.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-    const raccoon2 = await loadGLTF('/static/assets/models/musicband-raccoon/scene.gltf');
-    const raccoon = await loadGLTF('/static/assets/models/upch-dia/images (1).gltf');
-    //const chair = await loadGLTF('/static/assets/models/chair/scene.gltf');
+    const rana = await loadGLTF('/static/assets/models/rana/scene.gltf');
 
     //                      X     Y    Z
-    raccoon.scene.scale.set(0.01, 0.01, 0.01);
-    raccoon2.scene.scale.set(0.03, 0.03, 0.03);
+    rana.scene.scale.set(0.01, 0.01, 0.01);
     //                         X    Y   Z
-    raccoon.scene.position.set(0, -0.4, 0);
-    raccoon2.scene.position.set(0,-0.4,0);
+    rana.scene.position.set(0, -0.4, 0);
+    
 
-    const anchor_1 = mindarThree.addAnchor(0);
-    anchor_1.group.add(raccoon.scene);
-
-    const anchor_2 = mindarThree.addAnchor(1);
-    anchor_2.group.add(raccoon2.scene);
+    const anchor_rana = mindarThree.addAnchor(0);
+    anchor_rana.group.add(rana.scene);
 
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
